33633e10e1b41fc94c1a089257f82c6bf698ddab
Hina Isakai <2211140040e@ed.fuk.kindai.ac.jp>
2024-10-09 06:47:19 +0000

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "0727c7069c7d4489abccbcf561863fbd.vfs.cloud9.us-east-1.amazonaws.com"
 end
